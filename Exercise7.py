195867aba172f01d1e354c8350f72af4c296dafe
Rachit Jha <rachitjha20@gmail.com>
2024-07-31 23:44:26 +0530

    aadded new exercises

---
+++ Exercise7.py
@@ -0,0 +1,15 @@
+# Exercise 7: Print the following pattern
+# Write a program to use for loop to print the following reverse number pattern
+
+# 5 4 3 2 1 
+# 4 3 2 1 
+# 3 2 1 
+# 2 1 
+# 1
+
+
+for i in range(5, 0, -1):
+    for j in range(i):
+        print(i, end=" ")
+        i -= 1
+    print()
---
+++ Exercise8.py
@@ -0,0 +1,14 @@
+# Exercise 8: Print list in reverse order using a loop
+# Given:
+
+# 
+# Expected output:
+
+# 50
+# 40
+# 30
+# 20
+# 10
+list1 = [10, 20, 30, 40, 50]
+for i in list1[::-1]:
+    print(i)
