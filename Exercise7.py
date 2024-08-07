7c345c52b72af8b39aed52eb888a80abf25856dd
Rachit Jha <rachitjha20@gmail.com>
2024-08-07 22:50:05 +0530

    updated

--- Exercise7.py
+++ Exercise7.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Exercise 7: Print the following pattern
 # Write a program to use for loop to print the following reverse number pattern
 
@@ -13,20 +12,4 @@ for i in range(5, 0, -1):
     for j in range(i):
         print(i, end=" ")
         i -= 1
-=======
-# Exercise 7: Print the following pattern
-# Write a program to use for loop to print the following reverse number pattern
-
-# 5 4 3 2 1 
-# 4 3 2 1 
-# 3 2 1 
-# 2 1 
-# 1
-
-
-for i in range(5, 0, -1):
-    for j in range(i):
-        print(i, end=" ")
-        i -= 1
->>>>>>> 195867aba172f01d1e354c8350f72af4c296dafe
     print()
--- Exercise9.py
+++ Exercise9.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 # Exercise 9: Display numbers from -10 to -1 using for loop
 # Expected output:
 
@@ -14,21 +14,4 @@
 # -1
 
 for i in range(-10, 0, 1):
-=======
-# Exercise 9: Display numbers from -10 to -1 using for loop
-# Expected output:
-
-# -10
-# -9
-# -8
-# -7
-# -6
-# -5
-# -4
-# -3
-# -2
-# -1
-
-for i in range(-10, 0, 1):
->>>>>>> 195867aba172f01d1e354c8350f72af4c296dafe
     print(i)
